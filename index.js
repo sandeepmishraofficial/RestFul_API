738b5324d6bde3e51e1ee29bd6206cf51b470d29
SANDEEP MISHRA <sandeepmishrawork@outlook.com>
2024-03-07 07:33:17 +0530

    practice
    add code

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
-const { v4: uuidv4 } = require('uuid');
+const {v4: uuidv4} = require('uuid');
+ const  methodOverride = require('method-override')
 
-app.use(express.urlencoded({ extended: true }));
-
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
@@ -30,7 +31,7 @@ let posts = [
 ];
 
 app.get("/posts", (req, res) => {
-    res.render("index.ejs", { posts });
+    res.render("index.ejs", {posts});
 });
 
 app.get("/posts/new", (req, res) => {
@@ -38,27 +39,37 @@ app.get("/posts/new", (req, res) => {
 });
 
 app.post("/posts", (req, res) => {
-    let { username, content } = req.body;
+    let {username, content} = req.body;
     let id = uuidv4();
-    posts.push({ id, username, content });
+    posts.push({id, username, content});
     res.redirect("/posts");
 });
 
 app.get("/posts/:id", (req, res) => {
-    let { id } = req.params;
+    let {id} = req.params;
     let post = posts.find((p) => p.id === id);
-    res.render("show.ejs", { post });
+    res.render("show.ejs", {post});
 });
 
 app.patch("/posts/:id", (req, res) => {
-    let { id } = req.params;
+    let {id} = req.params;
     let newContent = req.body.content;
     let post = posts.find((p) => p.id === id);
     post.content = newContent;
     console.log(post);
     console.log(id);
-    res.send("patch request working");
+    res.redirect("/posts");
 });
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((p) => p.id === id);
+    res.render("edit.ejs",{post})
+});
+app.delete("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    posts = posts.filter((p) => p.id !== id);
+    res.redirect("/posts");
+})
 
 app.listen(port, () => {
     console.log("listening on port : 8080");
